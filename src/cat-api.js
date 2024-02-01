8c0fa7bdd8e6ffdcc4fdc0eaeffcb6d04b3700b8
czke33 <czke33@gmail.com>
2024-02-01 18:40:47 +0200

    add cat api

---
+++ src/cat-api.js
@@ -0,0 +1,22 @@
+const URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_ytXKzJTex2qoysuMUuCH9GUih58jBe3Ubxub8Orh1oQXsuEnp3IhJTfredNjkz3m';
+export function fetchBreeds() {
+  return fetch(`${URL}/breeds?api_key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `${URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
