e61f521c041f9c72d3094b1002ef7ed8dd660625
czke33 <czke33@gmail.com>
2024-02-01 21:22:15 +0200

    add style css to slimselect

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,8 +1,8 @@
 const URL = 'https://api.thecatapi.com/v1';
-const API_KEY =
+const CAT_API_KEY =
   'live_ytXKzJTex2qoysuMUuCH9GUih58jBe3Ubxub8Orh1oQXsuEnp3IhJTfredNjkz3m';
 export function fetchBreeds() {
-  return fetch(`${URL}/breeds?api_key=${API_KEY}`).then(response => {
+  return fetch(`${URL}/breeds?api_key=${CAT_API_KEY}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
@@ -12,7 +12,7 @@ export function fetchBreeds() {
 
 export function fetchCatByBreed(breedId) {
   return fetch(
-    `${URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+    `${URL}/images/search?api_key=${CAT_API_KEY}&breed_ids=${breedId}`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -1,29 +1,66 @@
 import SlimSelect from 'slim-select';
+//https://blog.corsego.com/stimulus-slim-select
+import 'slim-select/dist/slimselect.css';
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_ytXKzJTex2qoysuMUuCH9GUih58jBe3Ubxub8Orh1oQXsuEnp3IhJTfredNjkz3m';
 
-const apiKey =
-  'live_ytXKzJTex2qoysuMUuCH9GUih58jBe3Ubxub8Orh1oQXsuEnp3IhJTfredNjkz3m';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const url1 = 'https://api.thecatapi.com/v1/breeds';
-const url2 = 'https://api.thecatapi.com/v1/images';
+const selectEl = document.querySelector('.breed-select');
+const textMarkEl = document.querySelector('.cat-info');
+const loaderEl = document.querySelector('.loader');
+const errorEl = document.querySelector('.error');
 
-const fetchBreeds = () => {
-  return fetch(`${url1}?api_key=${apiKey}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-};
-const fetchCatByBreed = breedId => {
-  return fetch(`${url2}/${breedId}?api_key=${apiKey}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-};
+textMarkEl.classList.add('is-hidden');
+errorEl.classList.add('is-hidden');
+
+selectEl.addEventListener('change', createMarkUp);
+
+updateSelect();
+
+function updateSelect(data) {
+  fetchBreeds(data)
+    .then(data => {
+      loaderEl.classList.toggle('loader', 'is-hidden');
+
+      let markSelect = data.map(({ name, id }) => {
+        return `<option value ='${id}'>${name}</option>`;
+      });
+      selectEl.insertAdjacentHTML('beforeend', markSelect);
+      new SlimSelect({
+        select: selectEl,
+      });
+    })
+    .catch(onFetchError);
+}
+
+function createMarkUp(event) {
+  loaderEl.classList.toggle('is-hidden', 'loader');
+  selectEl.classList.add('is-hidden');
+  textMarkEl.classList.add('is-hidden');
+
+  const breedId = event.currentTarget.value;
+
+  fetchCatByBreed(breedId)
+    .then(data => {
+      loaderEl.classList.toggle('loader', 'is-hidden');
+      selectEl.classList.remove('is-hidden');
+      const { url, breeds } = data[0];
+
+      textMarkEl.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400"/><div class="box"><h2>${breeds[0].name}</h2><p>${breeds[0].description}</p><p><strong>Temperament:</strong> ${breeds[0].temperament}</p></div>`;
+      textMarkEl.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
+}
+
+function onFetchError() {
+  selectEl.classList.remove('is-hidden');
+  loaderEl.classList.toggle('loader', 'is-hidden');
+
+  new Notiflix.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!'
+  );
+}
