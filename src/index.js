51ac03068da481226c1267828748cb2f4cecbae8
czke33 <czke33@gmail.com>
2024-02-01 12:33:29 +0200

    add  fetch and api

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
+const apiKey =
+  'live_ytXKzJTex2qoysuMUuCH9GUih58jBe3Ubxub8Orh1oQXsuEnp3IhJTfredNjkz3m';
+
+const url1 = 'https://api.thecatapi.com/v1/breeds';
+const url2 = 'https://api.thecatapi.com/v1/images';
+
+const fetchBreeds = () => {
+  return fetch(`${url1}?api_key=${apiKey}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
+const fetchCatByBreed = breedId => {
+  return fetch(`${url2}/${breedId}?api_key=${apiKey}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
